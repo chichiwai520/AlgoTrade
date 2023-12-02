ad9aae16e856b5be9487b7d60c2b27c024d6a7b6
chichiwai520 <chichiwai520@gmail.com>
2023-12-01 23:44:36 -0800

    Version 2. Rule out Imbalance <0.25 and added William % R as support evidence

--- source/main/Strategy_imb_overtaken.py
+++ source/main/Strategy_imb_overtaken.py
@@ -2,11 +2,19 @@ import numpy as np
 import mplfinance as mpf
 from Data_fetch_from_TradingView import fetch_from_Tv
 from telegram_post import send_results_to_tg
-
+import talib
 
 
 def detect_imb_overtaken_signal(ticker,exchange,timeframe):
     df=fetch_from_Tv(ticker,exchange,timeframe)
+    high=df["High"]
+    low=df["Low"]
+    close=df["Close"]##########20231201 WILLIAM R %
+
+
+
+    df['ampitude'] = (df["Open"]-df["Close"]).abs()
+    df["WilliamR"] = talib.WILLR(high, low, close, timeperiod=14)   
     df['Bullish_Imbalance'] = False  # Initialize the new column
     df['Bearish_Imbalance'] = False  # Initialize the new column
     df["Bullish_imb_high"]= False
@@ -47,69 +55,90 @@ def detect_imb_overtaken_signal(ticker,exchange,timeframe):
         last_bull=df.at[i,"last_bull_imb"]
         PivotHigh=df.loc[last_bull:i,"High"].max()
         return PivotHigh+spread
-
+    def check_pivot_williamR(i):
+        last_bear=df.at[i,"last_bear_imb"]
+        if df.loc[last_bear:i,"WilliamR"].min() <= -75:
+          df.at[i,"WilliamOverSoldInPivot"] = 1
+        else:
+          df.at[i,"WilliamOverSoldInPivot"] = -1
+    def check_pivot_williamR_Be(i):
+        last_bull=df.at[i,"last_bull_imb"]
+        if df.loc[last_bull:i,"WilliamR"].max() >= -25:
+          df.at[i,"WilliamOverBoughtInPivot"] = 1
+        else:
+          df.at[i,"WilliamOverBoughtInPivot"] = -1
     
-    for i in range(2, len(df) - 1):##Strategy main
+    
         # Step 1: Find Strong & Normal Bullish Imbalance
+    for i in range(2, len(df) - 1):
         if df['Low'].iloc[i] > df['High'].iloc[i - 2]:
             df.at[i-1, 'Bullish_Imbalance'] = True
             df.at[i-1, 'Bullish_imb_high'] = df['Low'].iloc[i]
             df.at[i-1, 'Bullish_imb_low'] = df['High'].iloc[i - 2]
-            bear_imb_i=df.loc[:i,"Bearish_Imbalance"][df["Bearish_Imbalance"]].index # The later one [df["Bearish_Imbalance"]] is using as True.
-            if len(bear_imb_i)>0:
-                last_bear_imb_i=bear_imb_i[-1]
-                df.at[i,"last_bear_imb"] = last_bear_imb_i # index of prev. Be-Imb 4991
-                # To check if it is a strong Bull-Imb. (i.e. Imb High >　prev. Bear Imb High.)
-                if df.at[i-1,'Bullish_imb_high'] > df.at[last_bear_imb_i,"Bearish_imb_high"]:
-                    df.at[i,"Strong_bullish_imb_confirm"] = True
-                    check_bu_df=df[(df["Strong_bullish_imb_confirm"] == True) & (df["last_bear_imb"] == last_bear_imb_i)]
-                    if check_bu_df.shape[0] == 1:
-                        df.at[i,"Strong_Bullish_overtaken_bearish_imb_signal"] = True
-                        df.at[i-1,"mpfColour"] = "green"
-                        df.at[last_bear_imb_i,"mpfColour"] = "red"
-                        sl=findPivotLow(i)
-                        df.at[i,"Signal_S/L"] = sl
-                
-
+            df.at[i-1,"Bullish_imb_amp"]=df.at[i-1,'Bullish_imb_high']-df.at[i-1,'Bullish_imb_low']##########20231201 Only Check out IMB.....
+            if df.at[i-1,"Bullish_imb_amp"]/df.at[i-1,"ampitude"] >= 0.25:
+                # print("TESTING123") #Exclude small IMB
+                bear_imb_i=df.loc[:i,"Bearish_Imbalance"][df["Bearish_Imbalance"]].index # The later one [df["Bearish_Imbalance"]] is using as True.
+                if len(bear_imb_i)>0:
+                    last_bear_imb_i=bear_imb_i[-1]
+                    df.at[i,"last_bear_imb"] = last_bear_imb_i # index of prev. Be-Imb 4991
+                    # To check if it is a strong Bull-Imb. (i.e. Imb High >　prev. Bear Imb High.)and Williams R %
+                    if df.at[i-1,'Close'] > df.at[last_bear_imb_i,"Bearish_imb_high"]:
+                        df.at[i,"Strong_bullish_imb_confirm"] = True
+                        check_pivot_williamR(i)
+                        if df.at[i,"WilliamOverSoldInPivot"] == 1:
+                            check_bu_df=df[(df["Strong_bullish_imb_confirm"] == True) & (df["last_bear_imb"] == last_bear_imb_i)]
+                            if (check_bu_df.shape[0] == 1 or df.at[i-1,"Strong_bullish_imb_confirm"] == True) and df.at[i-1,"WilliamR"] >= -25: #
+                                df.at[i,"Strong_Bullish_overtaken_bearish_imb_signal"] = True
+                                df.at[i-1,"mpfColour"] = "green"
+                                df.at[last_bear_imb_i,"mpfColour"] = "red"
+                                sl=findPivotLow(i)
+                                df.at[i,"Signal_SL"] = sl
+        else:
+            pass
 
         # Step 2: Find Strong & Normal Bearish Imbalance
         if df['High'].iloc[i] < df['Low'].iloc[i - 2]:
             df.at[i-1, 'Bearish_Imbalance'] = True
             df.at[i-1, 'Bearish_imb_high'] = df['Low'].iloc[i - 2]
             df.at[i-1, 'Bearish_imb_low'] = df['High'].iloc[i]
-            bull_imb_i=df.loc[:i,"Bullish_Imbalance"][df["Bullish_Imbalance"]].index # The later one [df["Bullish_Imbalance"]] is using as True.
-            if len(bull_imb_i)>0:
-                last_bull_imb_i=bull_imb_i[-1]
-                df.at[i,"last_bull_imb"] = last_bull_imb_i # index of prev. Bu-Imb 4991
-                # To check if it is a strong Bear-Imb. (i.e. Imb Low <　prev. Bull Imb Low.)
-                if df.at[i-1,'Bearish_imb_low'] < df.at[last_bull_imb_i,"Bullish_imb_low"]:
-                    df.at[i,"Strong_bearish_imb_confirm"] = True
-                    check_be_df=df[(df["Strong_bearish_imb_confirm"] == True) & (df["last_bull_imb"] == last_bull_imb_i)]
-                    if check_be_df.shape[0] == 1:
-                        df.at[i,"Strong_Bearish_overtaken_bullish_imb_signal"] = True
-                        df.at[i-1,"mpfColour"] = "red"
-                        df.at[last_bull_imb_i,"mpfColour"] = "green"
-                        sl=findPivotHigh(i)
-                        df.at[i,"Signal_S/L"] = sl
+            df.at[i-1,"Bearish_imb_amp"]=df.at[i-1,'Bearish_imb_high']-df.at[i-1,'Bearish_imb_low']
+            if df.at[i-1,"Bearish_imb_amp"]/df.at[i-1,"ampitude"] >= 0.25:
+                bull_imb_i=df.loc[:i,"Bullish_Imbalance"][df["Bullish_Imbalance"]].index # The later one [df["Bullish_Imbalance"]] is using as True.
+                if len(bull_imb_i)>0:
+                    last_bull_imb_i=bull_imb_i[-1]
+                    df.at[i,"last_bull_imb"] = last_bull_imb_i # index of prev. Bu-Imb 4991
+                    # To check if it is a strong Bear-Imb. (i.e. Imb Low <　prev. Bull Imb Low.)
+                    if df.at[i-1,'Close'] < df.at[last_bull_imb_i,"Bullish_imb_low"]:
+                        df.at[i,"Strong_bearish_imb_confirm"] = True
+                        check_pivot_williamR_Be(i)
+                        if df.at[i,"WilliamOverBoughtInPivot"] == 1:
+                            check_be_df=df[(df["Strong_bearish_imb_confirm"] == True) & (df["last_bull_imb"] == last_bull_imb_i)]
+                            if (check_be_df.shape[0] == 1 or df.at[i-1,"Strong_bearish_imb_confirm"] == True) and df.at[i-1,"WilliamR"] <= -75:
+                                df.at[i,"Strong_Bearish_overtaken_bullish_imb_signal"] = True
+                                df.at[i-1,"mpfColour"] = "red"
+                                df.at[last_bull_imb_i,"mpfColour"] = "green"
+                                sl=findPivotHigh(i)
+                                df.at[i,"Signal_SL"] = sl
 
     #Step 3. Calculation on S/L=================================
     import decimal
-    sl=df["Signal_S/L"].iloc[-2]
+    sl=df["Signal_SL"].iloc[-2]
     entry=df["Close"].iloc[-2]
     rrr=round(entry-sl,5)
     rrr2=rrr*2
     rrr3=rrr*3
     buy_ratio=f"TP ref: 1R:{entry+rrr},2R:{entry+rrr2},3R{entry+rrr3}"
     sell_ratio=f"TP Ref: 1R:{entry+rrr},2R:{entry+rrr3},3R{entry+rrr3}"
-    message_buy=f"BUY SIGNAL->PREV. BEAR IMB OVERTAKEN\nTicker:{ticker}\nTimeframe:{timeframe}\nS/L={sl}\n{buy_ratio}\nPlease check the trend direction with Q and V to decide to take or not!!"
-    message_sell=f"SELL SIGNAL->PREV. BULL IMB OVERTAKEN\nTicker:{ticker}\nTimeframe:{timeframe}\nS/L={sl}\n\{sell_ratio}\nPlease check the trend direction with Q and V to decide to take or not!!"  
+    message_buy=f"Bullish_V_Detected\nTicker:{ticker}\nTimeframe:{timeframe}\nS/L={sl}\n{buy_ratio}\n [1 DEC] Revision on signal to rule out IMB / AMPT <0.25,added confirmation with William % Range"
+    message_sell=f"Bearish_V_Detected\nTicker:{ticker}\nTimeframe:{timeframe}\nS/L={sl}\n\{sell_ratio}\n [1 DEC] Revision on signal to rule out IMB / AMPT <0.25,added confirmation with William % Range"  
     #======================Results Action =====================================
     if df["Strong_Bullish_overtaken_bearish_imb_signal"].iloc[-2] == True:
-        sl=sl=df["Signal_S/L"].iloc[-2]
+        sl=sl=df["Signal_SL"].iloc[-2]
         plotting_graph(df,sl)
         send_results_to_tg(message_buy,open("testsave.png", 'rb'))
     if df["Strong_Bearish_overtaken_bullish_imb_signal"].iloc[-2] == True: 
-        sl=sl=df["Signal_S/L"].iloc[-2]
+        sl=sl=df["Signal_SL"].iloc[-2]
         plotting_graph(df,sl)
         send_results_to_tg(message_sell,open("testsave.png", 'rb')) 
     print("finished results check")  
